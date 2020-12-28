306fd9517051d8cca96d4bff6179afd20a342123
901david <vdavidhammond@gmail.com>
2020-12-28 12:15:01 -0700

    updated types, removed absract values support, and changed api to  only accept arrays

--- src/ProtectedMap.js
+++ src/ProtectedMap.js
@@ -1,85 +1,66 @@
-import * as React from "react";
-
-class ProtectedMap {
-  constructor(data, config) {
-    this.data = data;
-    this.map = this.convertToMap(data);
-    this.config = config;
-
-    this.modifyMappedState = this.modifyMappedState.bind(this);
-    this.getValue = this.getValue.bind(this);
-    this.setValue = this.setValue.bind(this);
-  }
-
-  getReturnValues() {
-    const { complexKeysEnabled } = this.config;
-    if (complexKeysEnabled) {
-      return [this.getValue, this.setValue];
+"use strict";
+exports.__esModule = true;
+exports.ProtectedMap = void 0;
+var React = require("react");
+var ProtectedMap = /** @class */ (function () {
+    function ProtectedMap(data) {
+        this.data = data;
+        this.map = this.convertToMap(this.data);
+        this.modifyMappedState = this.modifyMappedState.bind(this);
+        this.getValue = this.getValue.bind(this);
     }
-
-    const values = [...this.map.entries()].reduce((values, [key, map]) => {
-      return { ...values, [key]: map.get(key) };
-    }, {});
-    return [values, this.modifyMappedState];
-  }
-
-  convertToMap(data) {
-    const dataToProcess = Array.isArray(data) ? data : Object.entries(data);
-    const map = new Map();
-    dataToProcess.map(([key, val]) => {
-      const [stateVal, stateSetter] = React.useState(val);
-      map.set(
-        key,
-        new Map([
-          [key, stateVal],
-          ["stateSetter", stateSetter]
-        ])
-      );
-    });
-    return map;
-  }
-
-  modifyMappedState(prop, val) {
-    const data = this.map.get(prop);
-    const setter = data.get("stateSetter");
-    setter(val);
-  }
-
-  getValue(key) {
-    if (this.map.has(key)) {
-      return this.map.get(key).get(key);
-    }
-    return undefined;
-  }
-
-  setValue(keys, vals) {
-    // const isErrored =
-    //   (Array.isArray(keys) && !Array.isArray(vals)) ||
-    //   (!Array.isArray(keys) && Array.isArray(vals));
-    // if (isErrored)
-    //   throw new Error(
-    //     "To submit a batch request both keys and values must be arrays"
-    //   );
-    // const isBatched = Array.isArray(keys) && Array.isArray(vals);
-    // if (!isBatched) {
-    //   keys = [keys];
-    //   vals = [vals];
-    // }
-    // keys.forEach((key, idx) => {
-    //   if (this.map.has(key)) {
-    //     const innerMap = this.map.get(key);
-    //     const setter = innerMap.get("stateSetter");
-    //     setter(vals[idx]);
-    //   } else {
-    //     throw new Error("Key was not found in the map");
-    //   }
-    // });
-    if (this.map.has(key)) {
-      const innerMap = this.map.get(key);
-      const setter = innerMap.get("stateSetter");
-      setter(val);
-    }
-  }
-}
-
-export default ProtectedMap;
+    ProtectedMap.prototype.getReturnValues = function () {
+        var values = {};
+        for (var _i = 0, _a = this.map.entries(); _i < _a.length; _i++) {
+            var _b = _a[_i], key = _b[0], map = _b[1];
+            values[key] = map.get(key);
+        }
+        return [values, this.modifyMappedState];
+    };
+    ProtectedMap.prototype.convertToMap = function (data) {
+        if (!Array.isArray(data))
+            throw new Error("Intial State format must be [[key, initialState]]");
+        var map = new Map();
+        data.map(function (_a) {
+            var key = _a[0], val = _a[1];
+            var _b = React.useState(val), stateVal = _b[0], stateSetter = _b[1];
+            map.set(key, new Map([
+                [key, stateVal],
+                ["stateSetter", stateSetter],
+            ]));
+        });
+        return map;
+    };
+    ProtectedMap.prototype.modifyMappedState = function (keys, vals) {
+        var _this = this;
+        var isBatched = Array.isArray(keys) && Array.isArray(vals);
+        if (!isBatched) {
+            keys = [keys];
+            vals = [vals];
+        }
+        keys.forEach(function (key, idx) {
+            if (_this.map.has(key)) {
+                var innerMap = _this.map.get(key);
+                var setter = innerMap && innerMap.get("stateSetter");
+                if (setter) {
+                    setter(vals[idx]);
+                }
+                else
+                    throw new Error("State Setter not found");
+            }
+            else
+                throw new Error("Key was not found in the map");
+        });
+    };
+    ProtectedMap.prototype.getValue = function (key) {
+        if (this.map.has(key)) {
+            var stateMap = this.map.get(key);
+            if (stateMap !== undefined)
+                return stateMap.get(key);
+        }
+        else
+            throw new Error("Key not found, check your key name");
+    };
+    return ProtectedMap;
+}());
+exports.ProtectedMap = ProtectedMap;
---
+++ src/ProtectedMap.ts
@@ -0,0 +1,74 @@
+import * as React from "react";
+
+export type MappedStateEntry = Array<[string, unknown]>;
+export type valueSetter = (
+  reference: string | string[],
+  value: unknown | unknown[]
+) => void;
+
+export type MappedReturnValues = [{ [key: string]: unknown }, valueSetter];
+
+export type useMappedState = (
+  stateValues: MappedStateEntry
+) => MappedReturnValues;
+
+export class ProtectedMap {
+  private map: Map<string, Map<string, unknown>>;
+
+  constructor(private data: MappedStateEntry) {
+    this.map = this.convertToMap(this.data);
+    this.modifyMappedState = this.modifyMappedState.bind(this);
+    this.getValue = this.getValue.bind(this);
+  }
+
+  getReturnValues(): MappedReturnValues {
+    const values: { [key: string]: unknown } = {};
+    for (const [key, map] of this.map.entries()) {
+      values[key] = map.get(key);
+    }
+    return [values, this.modifyMappedState];
+  }
+
+  convertToMap(data: MappedStateEntry) {
+    if (!Array.isArray(data))
+      throw new Error("Intial State format must be [[key, initialState]]");
+    const map = new Map();
+
+    data.map(([key, val]) => {
+      const [stateVal, stateSetter] = React.useState(val);
+      map.set(
+        key,
+        new Map([
+          [key, stateVal],
+          ["stateSetter", stateSetter],
+        ])
+      );
+    });
+    return map;
+  }
+
+  modifyMappedState(keys: any | [any], vals: any | [any]) {
+    const isBatched = Array.isArray(keys) && Array.isArray(vals);
+    if (!isBatched) {
+      keys = [keys];
+      vals = [vals];
+    }
+
+    keys.forEach((key: string, idx: number) => {
+      if (this.map.has(key)) {
+        const innerMap = this.map.get(key);
+        const setter = innerMap && innerMap.get("stateSetter");
+        if (setter) {
+          (setter as (key: string) => void)(vals[idx]);
+        } else throw new Error("State Setter not found");
+      } else throw new Error("Key was not found in the map");
+    });
+  }
+
+  getValue(key: any) {
+    if (this.map.has(key)) {
+      const stateMap = this.map.get(key);
+      if (stateMap !== undefined) return stateMap.get(key);
+    } else throw new Error("Key not found, check your key name");
+  }
+}
